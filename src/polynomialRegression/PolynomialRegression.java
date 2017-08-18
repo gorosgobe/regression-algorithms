791c6e4e1357900b4e09ee536910855560a3e32a
Pablo Gorostiaga <pg1816@ic.ac.uk>
2017-08-18 17:55:44 +0200

    Implemented multithreaded optimal polynomial degree search

--- src/polynomialRegression/PolynomialRegression.java
+++ src/polynomialRegression/PolynomialRegression.java
@@ -4,7 +4,10 @@ package polynomialRegression;
 import Utils.MatrixUtils;
 import Utils.Point;
 
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class PolynomialRegression {
 
@@ -14,15 +17,20 @@ public class PolynomialRegression {
 
     /*
     Parameter column vector beta = (XTX)-1XTy where X is the design matrix.
-    Polynomial grade is number of columns of matrix
+    Polynomial degree is number of columns of matrix
      */
 
     public PolynomialRegression(List<Point> points, int polynomialDegree) {
         this.points = points;
-        if (polynomialDegree < 1) {
-            throw new IllegalArgumentException("Polynomial degree should be >= 1");
+        if (polynomialDegree < 0) {
+            throw new IllegalArgumentException("Polynomial degree should be >= 0");
         }
         this.polynomialDegree = polynomialDegree;
+    }
+
+    public PolynomialRegression(List<Point> points) {
+        this.points = points;
+        this.polynomialDegree = 0;
     }
 
     public List<Point> getPoints() {
@@ -92,6 +100,129 @@ public class PolynomialRegression {
 
         return result;
     }
-    
+
+    public int getOptimalPolynomialDegreeWithTestData(List<Point> testData) throws InterruptedException {
+
+        long startTime = System.nanoTime();
+
+        //gets max number of threads
+        int threadNum = Runtime.getRuntime().availableProcessors() - 1;
+        //creates an array of threads
+        final Thread[] threads = new Thread[threadNum];
+        //creates an array of polynomial degrees
+        final int[] threadPolyDegrees = new int[threadNum];
+        //creates an array of root mean squared errors
+        final double[] threadRMSE = new double[threadNum];
+
+        //for every available thread with index: threadIndex
+        IntStream.range(0, threadNum).forEach(threadIndex -> {
+            //create a new thread
+            Thread t = new Thread(() -> {
+                //with a mapping from RSMErrors to polynomial degrees
+                Map<Double, Integer> errorsToDegree = new LinkedHashMap<>();
+
+                //for the degrees corresponding to the thread index, compute the RSME errors and add it to the map
+                for (int i = ((testData.size() - 1) * threadIndex) / threadNum;
+                     i < (testData.size() - 1) * (threadIndex + 1)/ threadNum; i++) {
+
+                    PolynomialRegression regression = new PolynomialRegression(getPoints(), i);
+                    double error = regression.getTestDataRootMeanSquareError(testData);
+                    System.out.println("Degree: " + i + ", Error: " + error);
+                    errorsToDegree.put(error, i);
+
+                }
+
+                //for the degree section corresponding to the thread index, compute the minimum error and get its polynomial degree
+                threadRMSE[threadIndex] = Collections.min(errorsToDegree.keySet());
+                threadPolyDegrees[threadIndex] = errorsToDegree.get(threadRMSE[threadIndex]);
+
+            });
+
+            threads[threadIndex] = t;
+
+        });
+
+        //start threads
+        for (Thread t : threads) {
+            t.start();
+        }
+        //wait until the last thread to complete its process
+        for (Thread t : threads) {
+            t.join();
+        }
+
+        int minimalDegreeIndex = getIndexOfMinDouble(threadRMSE);
+
+        long endTime = System.nanoTime();
+
+        System.out.println("Time required: " + ((endTime - startTime) / 1000000000.0) + "s");
+        return threadPolyDegrees[minimalDegreeIndex];
+
+    }
+
+    private int getIndexOfMinDouble(double[] doubles) {
+
+        double current = Double.MAX_VALUE;
+        int currentIndex = 0;
+
+        for (int i = 0; i < doubles.length; i++) {
+            if (doubles[i] < current) {
+                current = doubles[i];
+                currentIndex = i;
+            }
+        }
+
+        return currentIndex;
+
+    }
+
+    public double getTrainingDataRootMeanSquareError() {
+        double meanSquareError =
+                points.stream()
+                        .map(i -> Math.pow(getPrediction(i.getX()) - i.getY(), 2.0))
+                        .reduce(0.0, (a, b) -> a + b) / points.size();
+
+        return Math.sqrt(meanSquareError);
+    }
+
+    public double getTestDataRootMeanSquareError(List<Point> testData) {
+        //model must be trained
+        if (coefficients == null) {
+            computeCoefficients();
+        }
+
+        double meanSquareError =
+                testData.stream()
+                        .map(i -> Math.pow(getPrediction(i.getX()) - i.getY(), 2.0))
+                        .reduce(0.0, (a, b) -> a + b) / testData.size();
+
+        return Math.sqrt(meanSquareError);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        File file = new File("src/testData2.txt");
+        Scanner sc = new Scanner(file);
+        sc.nextLine(); //ignores first line with comment
+        List<Point> points = new ArrayList<>();
+        List<Point> testData = new ArrayList<>();
+
+        int count = 0;
+        while (sc.hasNext()) {
+            //assume number of tokens is multiple of 2
+            String x = sc.next();
+            String y = sc.next();
+            if (count < 20) {
+                points.add(new Point(Integer.parseInt(x), Integer.parseInt(y)));
+            } else {
+                testData.add(new Point(Integer.parseInt(x), Integer.parseInt(y)));
+            }
+            count++;
+        }
+
+        PolynomialRegression regression = new PolynomialRegression(points, 0);
+        System.out.println(regression.getOptimalPolynomialDegreeWithTestData(testData));
+
+    }
+
 
 }
